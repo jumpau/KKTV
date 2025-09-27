d2dbd6b8303afac017ab5e8df40256e37a52d014
jumpau <jumpaulive@outlook.com>
2025-09-27 19:34:48 +0800

    Update route.ts

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,23 +1,55 @@
 import { NextResponse } from 'next/server';
 
-import { getAvailableApiSites, getCacheTime } from '@/lib/config';
+import { getCacheTime, getConfig } from '@/lib/config';
+import { searchFromApi } from '@/lib/downstream';
+import { yellowWords } from '@/lib/yellow';
 
 export const runtime = 'edge';
 
-// OrionTV 兼容接口
-export async function GET() {
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const query = searchParams.get('q');
+
+  if (!query) {
+    const cacheTime = await getCacheTime();
+    return NextResponse.json(
+      { results: [] },
+      {
+        headers: {
+          'Cache-Control': `public, max-age=${cacheTime}, s-maxage=${cacheTime}`,
+          'CDN-Cache-Control': `public, s-maxage=${cacheTime}`,
+          'Vercel-CDN-Cache-Control': `public, s-maxage=${cacheTime}`,
+        },
+      }
+    );
+  }
+
+  const config = await getConfig();
+  const apiSites = config.SourceConfig.filter((site) => !site.disabled);
+  const searchPromises = apiSites.map((site) => searchFromApi(site, query));
+
   try {
-    const apiSites = await getAvailableApiSites();
+    const results = await Promise.all(searchPromises);
+    let flattenedResults = results.flat();
+    if (!config.SiteConfig.DisableYellowFilter) {
+      flattenedResults = flattenedResults.filter((result) => {
+        const typeName = result.type_name || '';
+        return !yellowWords.some((word: string) => typeName.includes(word));
+      });
+    }
     const cacheTime = await getCacheTime();
 
-    return NextResponse.json(apiSites, {
-      headers: {
-        'Cache-Control': `public, max-age=${cacheTime}, s-maxage=${cacheTime}`,
-        'CDN-Cache-Control': `public, s-maxage=${cacheTime}`,
-        'Vercel-CDN-Cache-Control': `public, s-maxage=${cacheTime}`,
-      },
-    });
+    return NextResponse.json(
+      { results: flattenedResults },
+      {
+        headers: {
+          'Cache-Control': `public, max-age=${cacheTime}, s-maxage=${cacheTime}`,
+          'CDN-Cache-Control': `public, s-maxage=${cacheTime}`,
+          'Vercel-CDN-Cache-Control': `public, s-maxage=${cacheTime}`,
+        },
+      }
+    );
   } catch (error) {
-    return NextResponse.json({ error: '获取资源失败' }, { status: 500 });
+    return NextResponse.json({ error: '搜索失败' }, { status: 500 });
   }
 }
