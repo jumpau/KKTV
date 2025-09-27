bab3f04bcac021518492af6f35c2dc792c211a8f
jumpau <jumpaulive@outlook.com>
2025-09-27 19:03:40 +0800

    Update VideoCard.tsx

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { CheckCircle, Heart, Link, PlayCircleIcon } from 'lucide-react';
+import { CheckCircle, Heart, PlayCircleIcon } from 'lucide-react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
